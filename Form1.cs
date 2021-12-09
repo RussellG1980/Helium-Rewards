ef848a0b227b523c169df0f9143c67a320ac80c5
DavidDeeds <mistereadie@gmail.com>
2021-12-10 07:32:55 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helium_Rewards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var webClient = new System.Net.WebClient())
+            //using (var webClient = new System.Net.WebClient { Encoding = System.Text.Encoding.UTF8 })
+            {
+                webClient.Headers["User-Agent"] = "PostmanRuntime/7.28.4";
+                var json = webClient.DownloadString(textBox1.Text);
+                //var json = webClient.DownloadData(textBox1.Text);
+                // Now parse with JSON.Net
+                // http://jsonviewer.stack.hu/
+                //Console.WriteLine(json); - confirmed the above returns json
+
+                //ReadJSON.Rootobject record = JsonConvert.DeserializeObject<ReadJSON.Rootobject>(json);
+
+                //ReadJSON.Receipt recordReceipt = JsonConvert.DeserializeObject<ReadJSON.Receipt>(json);
+
+                //dynamic obj = Newtonsoft.Json.Linq.JObject.Parse(json);
+                //string results = obj.results;
+                //foreach (string result in results.Split())
+                //{
+                //    Console.WriteLine(result);
+                //}
+
+
+
+                //Console.WriteLine("JSON details");
+                ////Console.WriteLine(recordReceipt.origin);
+                //Console.WriteLine(recordReceipt.signal);
+                //Console.WriteLine(recordReceipt.snr);
+                //Console.WriteLine(recordReceipt.tx_power);
+                //Console.WriteLine();
+
+
+            }
+        }
+
+
+        //internal static bool IsValidJson(string data)
+        //{
+        //    data = data.Trim();
+        //    try
+        //    {
+        //        if (data.StartsWith("{") && data.EndsWith("}"))
+        //        {
+        //            Newtonsoft.Json.Linq.JToken.Parse(data);
+        //            Console.WriteLine("jToken - Parsing");
+   
+
+
+        //            Console.WriteLine();
+
+        //        }
+        //        else if (data.StartsWith("[") && data.EndsWith("]"))
+        //        {
+        //            Newtonsoft.Json.Linq.JArray.Parse(data);
+        //            Console.WriteLine("jArray - Parsing");
+        //            Console.WriteLine();
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+
+        //static async Task Main(string[] args)
+        //{
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        var json = await httpClient.GetStringAsync(textBox1.Text);
+        //        // Now parse with JSON.Net
+        //    }
+        //}
+    }
+}
---
+++ ReadJSON1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helium_Rewards
+{
+
+    public class Witness
+    {
+        public object timestamp { get; set; }
+        public double snr { get; set; }
+        public int signal { get; set; }
+        public string packet_hash { get; set; }
+        public string owner { get; set; }
+        public string location_hex { get; set; }
+        public string location { get; set; }
+        public bool is_valid { get; set; }
+        public string gateway { get; set; }
+        public double frequency { get; set; }
+        public string datarate { get; set; }
+        public int channel { get; set; }
+        public string invalid_reason { get; set; }
+    }
+
+    public class Receipt
+    {
+        public int tx_power { get; set; }
+        public object timestamp { get; set; }
+        public double snr { get; set; }
+        public int signal { get; set; }
+        public string origin { get; set; }
+        public string gateway { get; set; }
+        public double frequency { get; set; }
+        public object datarate { get; set; }
+        public string data { get; set; }
+        public int channel { get; set; }
+    }
+
+    public class Geocode
+    {
+        public string short_street { get; set; }
+        public string short_state { get; set; }
+        public string short_country { get; set; }
+        public string short_city { get; set; }
+        public string long_street { get; set; }
+        public string long_state { get; set; }
+        public string long_country { get; set; }
+        public string long_city { get; set; }
+        public string city_id { get; set; }
+    }
+
+    public class Path
+    {
+        public IList<Witness> witnesses { get; set; }
+        public Receipt receipt { get; set; }
+        public Geocode geocode { get; set; }
+        public string challengee_owner { get; set; }
+        public double challengee_lon { get; set; }
+        public string challengee_location_hex { get; set; }
+        public string challengee_location { get; set; }
+        public double challengee_lat { get; set; }
+        public string challengee { get; set; }
+    }
+
+    public class Datum
+    {
+        public string type { get; set; }
+        public int time { get; set; }
+        public string secret { get; set; }
+        public string request_block_hash { get; set; }
+        public IList<Path> path { get; set; }
+        public string onion_key_hash { get; set; }
+        public int height { get; set; }
+        public string hash { get; set; }
+        public int fee { get; set; }
+        public string challenger_owner { get; set; }
+        public double challenger_lon { get; set; }
+        public string challenger_location { get; set; }
+        public double challenger_lat { get; set; }
+        public string challenger { get; set; }
+    }
+
+    public class ReadJSON1
+    {
+        public IList<Datum> data { get; set; }
+        public string cursor { get; set; }
+    }
+
+}
